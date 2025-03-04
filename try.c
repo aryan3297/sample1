ed9ffcd60b3c936fa0b3d78c82405c463b836018
aryan3297 <rajatbhardwaj.x@gmail.com>
2025-03-04 09:41:10 +0530

    New line added for Test2 branch

--- try.c
+++ try.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
-int main()
 
+int multiply(int x, int y) {
+    return x * y;
+}
+
+int main()
 {
-	int a=10,b=20;
-	printf("%d";a+b);//comment added on remote only
-return 0;
+    int a = 10, b = 20;
+    printf("Sum: %d\n", a + b);
+    printf("Product: %d\n", multiply(a, b));
+    return 0;
 }
