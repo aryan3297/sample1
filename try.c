768ad01a7362e68b4f22c2c82b5a653c4af4d46b
aryan3297 <rajatbhardwaj.x@gmail.com>
2025-03-26 10:35:38 +0530

    committing after trashed

--- try.c
+++ try.c
@@ -3,7 +3,16 @@ int main()
 
 {
 	int a=10,b=20;
+<<<<<<< HEAD
 	printf("%d";a+b);
 	printf("HELLO");
+=======
+	printf("%d";a+b);//comment added on remote only
+	Hi i am added
+
+	Hi AUB in ASET dept.
+
+
+>>>>>>> 0e3f5a795d6373a20f68da6f0d7f3e8b63875d6f
 return 0;
 }
